31a1f7e9de1c25567f059d3cbcb3de790d00f986
mochalins <117967760+mochalins@users.noreply.github.com>
2024-04-19 16:15:12 +0900

    scm: Version bump to `0.1.0`

--- src/command.zig
+++ src/command.zig
@@ -329,7 +329,7 @@ fn help(params: [][]const u8) !void {
 
 fn version(_: [][]const u8) !void {
     // TODO: Figure out better way to get version from `build.zig.zon`.
-    std.log.info("CLI Version: {s}\n", .{"0.0.8"});
+    std.log.info("CLI Version: {s}\n", .{"0.1.0"});
 }
 
 fn set(params: [][]const u8) !void {
