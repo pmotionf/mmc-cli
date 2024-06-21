50d8c5e6700643cc0b62f611fc5ac339179d6b9c
mochalins <117967760+mochalins@users.noreply.github.com>
2024-06-21 14:01:10 +0900

    feat: set up configurator infra

---
+++ src/configurator.zig
@@ -0,0 +1,79 @@
+const std = @import("std");
+const args = @import("args");
+const Config = @import("Config.zig");
+
+const Options = struct {
+    create: bool = false,
+    help: bool = false,
+    output: []const u8 = "config.json",
+
+    pub const shorthands = .{
+        .c = "create",
+        .h = "help",
+        .o = "output",
+    };
+
+    pub const meta = .{
+        .option_docs = .{
+            .create = "create new configuration file",
+            .help = "print this help message",
+            .output = "configuration output path, defaults to \"config.json\"",
+        },
+    };
+};
+
+pub fn main() !u8 {
+    var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const stdout = std.io.getStdOut().writer();
+
+    const options = args.parseForCurrentProcess(
+        Options,
+        allocator,
+        .print,
+    ) catch return 1;
+    defer options.deinit();
+
+    if (options.options.help) {
+        try args.printHelp(
+            Options,
+            options.executable_name orelse "configurator",
+            stdout,
+        );
+        return;
+    }
+
+    var config: Config = .{
+        .modules = &.{},
+    };
+    // Load existing config file.
+    if (!options.options.create) {
+        if (options.positionals.len != 1) {
+            try args.printHelp(
+                Options,
+                options.executable_name orelse "configurator",
+                stdout,
+            );
+            return error.OneExistingConfigFilePathRequired;
+        }
+        var config_file = try std.fs.cwd().openFile(
+            options.positionals[0],
+            .{},
+        );
+        defer config_file.close();
+        config = Config.parse(allocator, config_file);
+    } else {
+        config.modules = try allocator.alloc(Config.Module, 1);
+        config.modules[0] = .{ .mcl = .{
+            .line_names = &.{},
+            .lines = &.{},
+        } };
+    }
+
+    // Main interaction loop.
+    while (true) {}
+
+    return 0;
+}
