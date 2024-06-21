6ab874e380f809b14068af9f401e3a8b02848afc
mochalins <117967760+mochalins@users.noreply.github.com>
2024-06-21 14:01:10 +0900

    feat: set up configurator infra

--- src/Config.zig
+++ src/Config.zig
@@ -4,7 +4,7 @@ const std = @import("std");
 const MclConfig = @import("command/mcl.zig").Config;
 const ReturnDemo2Config = @import("command/return_demo2.zig").Config;
 
-parsed: std.json.Parsed(Parse),
+modules: []ModuleConfig,
 
 pub const Module = enum {
     mcl,
@@ -16,28 +16,22 @@ const ModuleConfig = union(Module) {
     return_demo2: ReturnDemo2Config,
 };
 
-const Parse = struct {
-    modules: []ModuleConfig,
-};
-
-pub fn parse(allocator: std.mem.Allocator, f: std.fs.File) !Config {
+pub fn parse(
+    allocator: std.mem.Allocator,
+    f: std.fs.File,
+) !std.json.Parsed(Config) {
     var arena = std.heap.ArenaAllocator.init(std.heap.page_allocator);
     defer arena.deinit();
     const a = arena.allocator();
     const f_reader = f.reader();
     var json_reader = std.json.reader(a, f_reader);
 
-    const _result = try std.json.parseFromTokenSource(
-        Parse,
+    return try std.json.parseFromTokenSource(
+        Config,
         allocator,
         &json_reader,
         .{},
     );
-
-    const result = Config{
-        .parsed = _result,
-    };
-    return result;
 }
 
 pub fn modules(self: *Config) []const ModuleConfig {
