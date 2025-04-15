9873f46e822d2f5b86073dd5b31df31a532fcd47
aaumar25 <aaumar@pmotionf.com>
2025-04-15 15:08:46 +0900

    refactor: circularBuffer, `logging_data` type
    refactor: remove tail in ring buffer, test ring buffer

--- src/circular_buffer.zig
+++ src/circular_buffer.zig
@@ -8,7 +8,7 @@ pub fn CircularBuffer(comptime T: type) type {
 
         buffer: []T,
         head: usize,
-        tail: usize,
+        count: usize,
         allocator: std.mem.Allocator,
 
         /// Initialize circular buffer capacity. Capacity is not resizable.
@@ -17,7 +17,7 @@ pub fn CircularBuffer(comptime T: type) type {
             return Self{
                 .buffer = bytes,
                 .head = 0,
-                .tail = 0,
+                .count = 0,
                 .allocator = allocator,
             };
         }
@@ -29,77 +29,122 @@ pub fn CircularBuffer(comptime T: type) type {
         /// Writes item to tail of buffer. Errors if buffer is full. Does not
         /// allocate.
         pub fn writeItem(self: *Self, item: T) !void {
-            if ((self.tail + 1) % self.buffer.len == self.head)
+            if (self.count == self.buffer.len)
                 return error.BufferFull;
 
-            self.buffer[self.tail] = item;
-            self.tail = (self.tail + 1) % self.buffer.len;
+            if (@typeInfo(T) == .array) {
+                @memcpy(
+                    &self.buffer[(self.head + self.count) % self.buffer.len],
+                    &item,
+                );
+            } else {
+                self.buffer[(self.head + self.count) % self.buffer.len] = item;
+            }
+            self.count += 1;
         }
 
         /// Writes item to the tail of the buffer. Overwrites the oldest item if
         /// full. Does not allocate.
         pub fn writeItemOverwrite(self: *Self, item: T) void {
-            self.buffer[self.tail] = item;
-            self.tail = (self.tail + 1) % self.buffer.len;
-            if (self.tail == self.head)
-                self.head = (self.head + 1) % self.buffer.len;
-        }
-
-        /// Writes slice to the tail of the buffer. Overwrites the oldest slice if
-        /// full. Does not allocate.
-        pub fn writeSliceOverwrite(self: *Self, item: T) void {
-            @memcpy(self.buffer[self.tail], item);
-            self.tail = (self.tail + 1) % self.buffer.len;
-            if (self.tail == self.head)
-                self.head = (self.head + 1) % self.buffer.len;
+            if (@typeInfo(T) == .array) {
+                @memcpy(
+                    &self.buffer[(self.head + self.count) % self.buffer.len],
+                    &item,
+                );
+            } else {
+                self.buffer[(self.head + self.count) % self.buffer.len] = item;
+            }
+            if (self.count == self.buffer.len)
+                self.head = (self.head + 1) % self.buffer.len
+            else
+                self.count += 1;
         }
 
         /// Read next item from front of buffer. Advances buffer head.
         pub fn readItem(self: *Self) ?T {
-            if (self.tail == self.head) return null;
+            if (self.count == 0) return null;
 
             const item = self.buffer[self.head];
             self.head = (self.head + 1) % self.buffer.len;
+            self.count -= 1;
             return item;
         }
 
         /// Peek at next item from front of buffer. Does not advance head.
         pub fn peek(self: *Self) ?T {
-            if (self.tail == self.head) return null;
+            if (self.count == 0) return null;
             return self.buffer[self.head];
         }
 
         /// Discard up to first count items in buffer. Count must be less than
-        /// or equal to buffer length.
+        /// or equal to current used buffer length.
         pub fn discard(self: *Self, count: usize) void {
-            std.debug.assert(count <= self.buffer.len);
+            std.debug.assert(count <= self.count);
             const remove_count: usize = @min(count, self.items());
             self.head = (self.head + remove_count) % self.buffer.len;
+            self.count -= count;
         }
 
         /// Returns true if buffer is empty and false otherwise.
         pub fn isEmpty(self: Self) bool {
-            return self.tail == self.head;
+            return self.count == 0;
         }
 
         /// Returns true if buffer is full and false otherwise.
         pub fn isFull(self: Self) bool {
-            return ((self.tail + 1) % self.buffer.len == self.head);
+            return self.count == self.buffer.len;
         }
 
         /// Discard all items in buffer. Does not free/reset capacity.
         pub fn clearRetainingCapacity(self: *Self) void {
             self.head = 0;
-            self.tail = 0;
+            self.count = 0;
         }
 
         /// Get the total number of items in buffer.
         pub fn items(self: Self) usize {
-            if (self.tail >= self.head) {
-                return self.tail - self.head;
-            } else {
-                return self.buffer.len - self.head + self.tail;
-            }
+            self.count;
         }
     };
+}
+
+test "write array" {
+    const text1 = "Testing from the Earth";
+    const text2 = "Received by someone in the future";
+    const full_text1 = text1 ++ "\n" ++ text2;
+    const text3 = "Testing from the future";
+    const text4 = "Received by someone in the Earth";
+    const full_text2 = text3 ++ "\n" ++ text4;
+    var ring_buf =
+        try CircularBuffer([full_text1.len]u8).initCapacity(
+            std.heap.page_allocator,
+            1,
+        );
+
+    // test writeItem
+    try ring_buf.writeItem(full_text1.*);
+
+    var iterator1 =
+        std.mem.tokenizeSequence(
+            u8,
+            &ring_buf.readItem().?,
+            "\n",
+        );
+    try std.testing.expectEqualStrings(text1, iterator1.next().?);
+    try std.testing.expectEqualStrings(text2, iterator1.next().?);
+    try std.testing.expect(ring_buf.isEmpty());
+
+    // test writeItemOverwrite
+    ring_buf.writeItemOverwrite(full_text1.*);
+    try std.testing.expect(ring_buf.isFull());
+    ring_buf.writeItemOverwrite(full_text2.*);
+    var iterator2 =
+        std.mem.tokenizeSequence(
+            u8,
+            &ring_buf.readItem().?,
+            "\n",
+        );
+    try std.testing.expectEqualStrings(text3, iterator2.next().?);
+    try std.testing.expectEqualStrings(text4, iterator2.next().?);
+    try std.testing.expect(ring_buf.isEmpty());
 }
