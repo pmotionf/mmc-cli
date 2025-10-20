a840eb1fb5e8b0d687dae56f6471edb680017eba
aaumar25 <aaumar@pmotionf.com>
2025-10-20 14:09:55 +0900

    fix: support driver filter on `release_carrier`

--- src/command/mmc_client/callbacks.zig
+++ src/command/mmc_client/callbacks.zig
@@ -732,8 +732,52 @@ pub fn releaseCarrier(params: [][]const u8) !void {
                 if (axis.carrier == 0) return error.CarrierNotFound;
                 break :b axis.carrier;
             },
-            .driver => return error.InvalidParameter,
             .carrier => |carrier_id| break :b carrier_id[0],
+            .driver => {
+                {
+                    try client.removeIgnoredMessage(socket);
+                    try socket.waitToWrite(&command.checkCommandInterrupt);
+                    var writer = socket.writer(&client.writer_buf);
+                    try client.api.request.info.track.encode(
+                        client.allocator,
+                        &writer.interface,
+                        .{
+                            .line = line.id,
+                            .info_carrier_state = true,
+                            .filter = _filter.toProtobuf(),
+                        },
+                    );
+                    try writer.interface.flush();
+                }
+                try socket.waitToRead(&command.checkCommandInterrupt);
+                var reader = socket.reader(&client.reader_buf);
+                var track = try client.api.response.info.track.decode(
+                    client.allocator,
+                    &reader.interface,
+                );
+                defer track.deinit(client.allocator);
+                if (track.line != line.id) return error.InvalidResponse;
+                const carriers = track.carrier_state;
+                if (carriers.items.len == 0) return error.CarrierNotFound;
+                for (carriers.items) |carrier| {
+                    {
+                        try client.removeIgnoredMessage(socket);
+                        try socket.waitToWrite(&command.checkCommandInterrupt);
+                        var writer = socket.writer(&client.writer_buf);
+                        try client.api.request.command.release.encode(
+                            client.allocator,
+                            &writer.interface,
+                            .{
+                                .line = line.id,
+                                .carrier = carrier.id,
+                            },
+                        );
+                        try writer.interface.flush();
+                    }
+                    try waitCommandReceived(client.allocator);
+                }
+                return;
+            },
         }
     } else null;
     {
@@ -2207,6 +2251,7 @@ pub fn resumeLine(params: [][]const u8) !void {
     try waitCommandReceived(client.allocator);
 }
 
+/// Get the command ID and track that command until completed.
 fn waitCommandReceived(allocator: std.mem.Allocator) !void {
     const socket = client.sock orelse return error.ServerNotConnected;
     var id: u32 = 0;
