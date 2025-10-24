71aefe105d4b0c9dfbfa4f76677fa0dd4fb1e159
aaumar25 <aaumar@pmotionf.com>
2025-10-24 16:02:21 +0900

    refactor: utilize new api target

--- src/modules/mmc_client/commands/release_carrier.zig
+++ src/modules/mmc_client/commands/release_carrier.zig
@@ -12,84 +12,6 @@ pub fn impl(params: [][]const u8) !void {
     if (params[1].len > 0) {
         filter = try .parse(params[1]);
     }
-    const carrier_id: ?u32 = if (filter) |*_filter| b: {
-        switch (_filter.*) {
-            .axis => {
-                {
-                    try client.removeIgnoredMessage(socket);
-                    try socket.waitToWrite(&command.checkCommandInterrupt);
-                    var writer = socket.writer(&client.writer_buf);
-                    try client.api.request.info.track.encode(
-                        client.allocator,
-                        &writer.interface,
-                        .{
-                            .line = line.id,
-                            .info_axis_state = true,
-                            .filter = _filter.toProtobuf(),
-                        },
-                    );
-                    try writer.interface.flush();
-                }
-                try socket.waitToRead(&command.checkCommandInterrupt);
-                var reader = socket.reader(&client.reader_buf);
-                var track = try client.api.response.info.track.decode(
-                    client.allocator,
-                    &reader.interface,
-                );
-                defer track.deinit(client.allocator);
-                if (track.line != line.id) return error.InvalidResponse;
-                const axis = track.axis_state.pop() orelse return error.InvalidResponse;
-                if (axis.carrier == 0) return error.CarrierNotFound;
-                break :b axis.carrier;
-            },
-            .carrier => |carrier_id| break :b carrier_id[0],
-            .driver => {
-                {
-                    try client.removeIgnoredMessage(socket);
-                    try socket.waitToWrite(&command.checkCommandInterrupt);
-                    var writer = socket.writer(&client.writer_buf);
-                    try client.api.request.info.track.encode(
-                        client.allocator,
-                        &writer.interface,
-                        .{
-                            .line = line.id,
-                            .info_carrier_state = true,
-                            .filter = _filter.toProtobuf(),
-                        },
-                    );
-                    try writer.interface.flush();
-                }
-                try socket.waitToRead(&command.checkCommandInterrupt);
-                var reader = socket.reader(&client.reader_buf);
-                var track = try client.api.response.info.track.decode(
-                    client.allocator,
-                    &reader.interface,
-                );
-                defer track.deinit(client.allocator);
-                if (track.line != line.id) return error.InvalidResponse;
-                const carriers = track.carrier_state;
-                if (carriers.items.len == 0) return error.CarrierNotFound;
-                for (carriers.items) |carrier| {
-                    {
-                        try client.removeIgnoredMessage(socket);
-                        try socket.waitToWrite(&command.checkCommandInterrupt);
-                        var writer = socket.writer(&client.writer_buf);
-                        try client.api.request.command.release.encode(
-                            client.allocator,
-                            &writer.interface,
-                            .{
-                                .line = line.id,
-                                .carrier = carrier.id,
-                            },
-                        );
-                        try writer.interface.flush();
-                    }
-                    try client.waitCommandReceived();
-                }
-                return;
-            },
-        }
-    } else null;
     {
         try client.removeIgnoredMessage(socket);
         try socket.waitToWrite(&command.checkCommandInterrupt);
@@ -99,7 +21,17 @@ pub fn impl(params: [][]const u8) !void {
             &writer.interface,
             .{
                 .line = line.id,
-                .carrier = if (carrier_id) |carrier| carrier else null,
+                .target = if (filter) |f| b: switch (f) {
+                    .axis => |axis| break :b .{
+                        .axes = .{ .start = axis, .end = axis },
+                    },
+                    .driver => |driver| break :b .{
+                        .drivers = .{ .start = driver, .end = driver },
+                    },
+                    .carrier => |carrier| break :b .{
+                        .carrier = carrier[0],
+                    },
+                } else null,
             },
         );
         try writer.interface.flush();
