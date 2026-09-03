945ba8523dfb5753ce4b8cecffea55b66926e9bb
aaumar25 <aaumar@premorobo.com>
2026-09-03 16:50:41 +0900

    refactor: Remove continuous push

--- src/modules/mmc_client/commands/group.zig
+++ src/modules/mmc_client/commands/group.zig
@@ -86,113 +86,6 @@ pub fn impl(io: std.Io, gpa: std.mem.Allocator) !void {
                     .DIRECTION_BACKWARD
                 else
                     return error.InvalidDirection;
-            const carrier_id: ?u32 = if (params[3].len > 0) try std.fmt.parseInt(u10, b: {
-                const input = params[3];
-                var suffix: ?usize = null;
-                for (input, 0..) |c, i| if (!std.ascii.isDigit(c)) {
-                    // Only valid suffix for carrier id is either 'c' or "carrier".
-                    if (c != 'c') return error.InvalidCharacter;
-                    suffix = i;
-                    break;
-                };
-                if (suffix) |ignore_idx| {
-                    if (ignore_idx == 0) return error.InvalidCharacter;
-                    break :b input[0..ignore_idx];
-                } else break :b input;
-            }, 0) else null;
-            // If carrier is provided, send the specified carrier to the pushing axis
-            // with offset half of carrier length.
-            if (carrier_id) |id| check_move: {
-                // Continuous push need to move the target carrier to the target axis.
-                // Push activation condition is that the hall sensor on the push
-                // direction is on or the carrier waited by the pushing axis has
-                // movement target that makes the hall sensor on the push direction
-                // to be on. Then, the carrier will be pushed to the direction once
-                // it is detected by the target axis.
-                //
-                // Thus, if the target carrier is already on the target axis and the
-                // hall sensor of the push direction is already on, the move command is
-                // not necessary.
-                check_carrier: {
-                    var line_array: [1]u32 = .{line.id};
-                    const lines: std.ArrayList(u32) = .fromOwnedSlice(&line_array);
-                    // This blocks checks whether the move command is necessary or not.
-                    const request: api.protobuf.mmc.Request = .{
-                        .body = .{
-                            .info = .{
-                                .body = .{
-                                    .track = .{
-                                        .lines = lines,
-                                        .info_axis_state = true,
-                                        .filter = .{
-                                            .axes = .{
-                                                .start = axis_id,
-                                                .end = axis_id,
-                                            },
-                                        },
-                                    },
-                                },
-                            },
-                        },
-                    };
-                    try client.sendRequest(io, gpa, net, request);
-                    var decoded = try client.getResponse(gpa, io, net);
-                    defer decoded.deinit(gpa);
-                    const track = switch (decoded.body orelse return error.InvalidResponse) {
-                        .info => |info_resp| switch (info_resp.body orelse
-                            return error.InvalidResponse) {
-                            .track => |track_resp| track_resp,
-                            .request_error => |req_err| {
-                                return client.error_response.throwInfoError(req_err);
-                            },
-                            else => return error.InvalidResponse,
-                        },
-                        .request_error => |req_err| {
-                            return client.error_response.throwMmcError(req_err);
-                        },
-                        else => return error.InvalidResponse,
-                    };
-
-                    if (track.lines.items.len != 1)
-                        return error.InvalidResponse;
-
-                    const track_line = &track.lines.items[0];
-                    if (track_line.id != line.id)
-                        return error.InvalidResponse;
-
-                    const axis_state = track_line.axis_state.pop() orelse
-                        return error.InvalidResponse;
-                    // If no carrier is detected, continue to move the specified carrier
-                    // to the pushing axis.
-                    const carrier = track_line.carrier_state.pop() orelse
-                        break :check_carrier;
-                    if (dir == .DIRECTION_FORWARD and axis_state.hall_alarm_front or
-                        dir == .DIRECTION_BACKWARD and axis_state.hall_alarm_back)
-                    {
-                        // Move command is not necessary in the following condition.
-                        if (carrier.id == id) break :check_move;
-                    }
-                }
-                const location =
-                    line.length.axis * @as(f32, @floatFromInt(axis_id - 1)) +
-                    switch (dir) {
-                        .DIRECTION_BACKWARD => -line.length.axis / 2.0,
-                        .DIRECTION_FORWARD => line.length.axis / 2.0,
-                        else => unreachable,
-                    };
-                try commands.append(gpa, .{
-                    .command = .{
-                        .move = .{
-                            .line = line.id,
-                            .carrier = id,
-                            .velocity = line.velocity,
-                            .acceleration = line.acceleration,
-                            .target = .{ .location = location },
-                            .control = .CONTROL_POSITION,
-                        },
-                    },
-                });
-            }
             // Push command request
             try commands.append(gpa, .{
                 .command = .{
@@ -202,7 +95,6 @@ pub fn impl(io: std.Io, gpa: std.mem.Allocator) !void {
                         .acceleration = line.acceleration,
                         .direction = dir,
                         .axis = axis_id,
-                        .carrier = carrier_id,
                     },
                 },
             });
